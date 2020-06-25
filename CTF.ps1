cd34f9801ffdd128ea61d22fb71e0d6e3f8a58c6
Doug McGaughy <dmcgaughy@gmail.com>
2020-06-24 21:17:48 -0500

    First upload to git

---
+++ CTF.ps1
@@ -0,0 +1,7 @@
+﻿[xml]$DATA=get-content '.\sheet1.xml' 
+foreach ($row in $DATA.worksheet.sheetData.row){
+    foreach ($column in $row.c[5]){
+        Write-Host $column.v
+        }
+        
+    }
