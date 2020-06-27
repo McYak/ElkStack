0c04a7fae501afbbc51d7d59bb2a8061f269e28b
Doug McGaughy <dmcgaughy@gmail.com>
2020-06-27 14:29:29 -0500

    "up

--- CTF.ps1
+++
@@ -1,7 +0,0 @@
-﻿[xml]$DATA=get-content '.\sheet1.xml' 
-foreach ($row in $DATA.worksheet.sheetData.row){
-    foreach ($column in $row.c[5]){
-        Write-Host $column.v
-        }
-        
-    }
